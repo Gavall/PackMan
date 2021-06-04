cc6c05fa6c4982f84f75315a982a9580a22273ed
Maxim Khabarov <M.k.invest@yandex.ru>
2021-06-04 21:37:05 +0300

    first commit
    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$root = $_SERVER['DOCUMENT_ROOT'];
+include($root.'/header.php'); ?>
+<link rel="stylesheet" href="css/style.css">
+    <section>
+        <div class="container">
+
+        </div>
+    </section>
+    <script src="ghosts.js"></script>
+    <script src="js.js"></script>
+<?php include($root.'/footer.php'); ?>
